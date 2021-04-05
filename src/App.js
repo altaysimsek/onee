8ee4211115667854d18330d65c4b1611da6e8ef4
Altay Simsek <altaysimsek16@gmail.com>
2021-04-05 23:52:06 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import { Layout, Row, Col, Divider, Image } from 'antd';
+import { Layout, Row, Col, Divider, Image, Form, Radio, Input } from 'antd';
 import Navbar from './components/Navbar';
-const { Content } = Layout;
+import {
+    PhoneOutlined,
+    LinkedinFilled,
+    InstagramFilled,
+    FacebookFilled,
+    TwitterSquareFilled,
+    YoutubeFilled,
+} from '@ant-design/icons';
+const { Content, Footer } = Layout;
 
 function App() {
     return (
@@ -98,7 +106,111 @@ function App() {
                     </ul>
                 </Row>
                 <Divider />
+                <h2 style={{ textAlign: 'center', fontSize: '42px' }}>
+                    Invite Fellows to Register
+                </h2>
+                <Row className='form-container'>
+                    <Form></Form>
+                </Row>
             </Content>
+            <Footer>
+                <Row className='upper-footer' gutter={16}>
+                    <Col className='gutter-row' sm={6} span={24}>
+                        <p>COMPANY</p>
+                        <ul className='list-footer'>
+                            <li>
+                                <a href='/about'>About Us</a>
+                            </li>
+                            <li>
+                                <a href='/agreement/privacy-policy'>Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href='/agreement/user-terms-conditions'>
+                                    ONEE Luxury Travel Terms of Use
+                                </a>
+                            </li>
+                            <li>
+                                <a href='/travel-guide'>Travel Guide</a>
+                            </li>
+                            <li>
+                                <a href='/we-in-press'>ONEE in the Press</a>
+                            </li>
+                            <li>
+                                <a href='/competition'>Competition</a>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col className='gutter-row' sm={6} span={24}>
+                        <p>HOW TO WORK WITH US</p>
+                        <ul className='list-footer'>
+                            <li>
+                                <a href='/property-managers'>Vacation Rental Property Managers</a>
+                            </li>
+                            <li>
+                                <a href='/travel-agents'>Travel Agents</a>
+                            </li>
+                            <li>
+                                <a href='/guests'>Guests</a>
+                            </li>
+                            <li>
+                                <a href='/service-partners'>Service Partners</a>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col className='gutter-row' sm={6} span={24}>
+                        <p>GENERAL INQUIRIES</p>
+                        <ul className='list-footer'>
+                            <li>
+                                <a href='/working-at-onee'>Working at ONEE</a>
+                            </li>
+                            <li>
+                                <a href='/send-us-an-email'>Send Us an Email</a>
+                            </li>
+                            <li>
+                                <a href='/faq/default'>FAQ</a>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col className='gutter-row' sm={6} span={24}>
+                        <p>CONTACT US</p>
+                        <ul className='list-footer'>
+                            <li>
+                                LONDON <br /> ONEE Travel Services Ltd <br /> Newton House <br />{' '}
+                                118 Piccadilly <br /> Mayfair <br /> W1J 7NW
+                            </li>
+                            <li>
+                                <PhoneOutlined /> LONDON{' '}
+                                <a href='tel:+44 20 8142 4801'>+44 20 8142 4801</a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Divider></Divider>
+                <Row>
+                    <Col sm={18} span={24}>
+                        © 2021 All Rights Reserved to ONEE Luxury Travel.
+                    </Col>
+                    <Col sm={6} span={24}>
+                        <ul className='social-links'>
+                            <li>
+                                <LinkedinFilled style={{ fontSize: '24px' }} />
+                            </li>
+                            <li>
+                                <InstagramFilled style={{ fontSize: '24px' }} />
+                            </li>
+                            <li>
+                                <FacebookFilled style={{ fontSize: '24px' }} />
+                            </li>
+                            <li>
+                                <TwitterSquareFilled style={{ fontSize: '24px' }} />
+                            </li>
+                            <li>
+                                <YoutubeFilled style={{ fontSize: '24px' }} />
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Footer>
         </Layout>
     );
 }
